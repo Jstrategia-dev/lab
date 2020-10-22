f853a22edd1266fab2a8d262f91cd5a4e9287912
Keven Foster <kfoster@jstrategia.com>
2020-10-22 02:25:58 -0400

    add files

---
+++ src/fablab-equipe1.cpp
@@ -0,0 +1,80 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/mnt/c/src/fablab/fablab-equipe1/src/fablab-equipe1.ino"
+// This #include statement was automatically added by the Particle IDE.
+// This #include statement was automatically added by the Particle IDE.
+#include <MQTT.h>
+
+void MQTTcallback(char* topic, byte* payload, unsigned int length);
+void handler(const char *topic, const char *data);
+void setup();
+void loop();
+#line 5 "/mnt/c/src/fablab/fablab-equipe1/src/fablab-equipe1.ino"
+int btnPin = A4;                                                    // tell the program where the button is connected to              
+int ledPin = D7;                                                    // tell the program where the on-board LED is connected to           
+int currentBtnState = -1;                                           // LOW = idle, HIGH = pressed
+int prevBtnState = -1;
+char deviceName[32] = "";
+char msg[50] = "Hello ! ";
+char mainTopic[20] = "Mercaptan/";
+char fullTopic[60] = "";
+unsigned long lastTime = 0;
+
+
+//MQTT client("test.mosquitto.org", 1883, MQTTcallback);             // we will be using the public Mosquitto MQTT server
+MQTT client("207.253.185.55", 1883, MQTTcallback);                 // we will be using the public Eclipse MQTT server
+// byte server1[] = {192,168,1,84};
+// MQTT client(server1,1883,MQTTcallback);
+void MQTTcallback(char* topic, byte* payload, unsigned int length)   // will be called if a message is posted to a MQTT topic we subscribed to
+{
+   
+}
+
+void handler(const char *topic, const char *data)                    // This is called when the Cloud makes the current device name available
+{
+  strncpy(deviceName, data, sizeof(deviceName)-1);
+  strcat(fullTopic,mainTopic);
+  strcat(fullTopic,deviceName);
+}
+
+void setup()
+{
+  Particle.subscribe("particle/device/name", handler);               // prepare to retrieve current device public name
+  Particle.publish("particle/device/name");                          // ask the cloud for the name to be sent to you
+  pinMode(ledPin, OUTPUT);                                           // configure the on-board LED
+  pinMode(btnPin, INPUT_PULLDOWN);                                   // tell the program we will read the button state soon
+}
+
+void loop()
+{
+    // connect to MQTT broker
+    if (!client.isConnected())
+    {
+       if (!client.connect("MyPhoton"))
+       {
+           digitalWrite(ledPin, HIGH);                                 // turn Photon LED on, indicating we are unable to connect
+           delay(1000);
+           return;
+       }
+    }
+    digitalWrite(ledPin, LOW);                                         // turn Photon LED off
+    unsigned long now = millis();                                      
+    currentBtnState = digitalRead(btnPin);                             // read the emergency button state 
+    if (currentBtnState == HIGH && prevBtnState != currentBtnState)    // the button has been pressed
+    {
+      if ((now - lastTime) >= 1000)                                    // we don't want to send more than one message per second
+      {
+        digitalWrite(ledPin, HIGH);                                    // turn Photon LED on, indicating we are sending a message
+        client.publish(fullTopic, msg);                                    // send a message to the MQTT server
+        delay(100);                                                    // to give some time to see the LED on     
+        lastTime = now;                                                               
+        digitalWrite(ledPin, LOW);                                     // turn Photon LED off, confirming the message has been sent
+      }
+    }
+    prevBtnState = currentBtnState;                                    // to keep track off button state for next iteration
+    client.loop();                                                     // this is required to ensure the MQTT stuff works
+}
+
